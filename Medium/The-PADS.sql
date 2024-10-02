ccfcf78d21e3e068f0cf433426dc9f72c55fb1bc
Tanmay Unhale <83015537+Tanmay9993@users.noreply.github.com>
2024-10-02 17:00:48 -0400

    Create The-PADS.sql

---
+++ Medium/The-PADS.sql
@@ -0,0 +1,30 @@
+select concat(name,'(',left(occupation,1),')') from occupations
+order by name asc;
+
+select concat('There are a total of ', count(Occupation), ' ' ,lower(occupation),'s.') from occupations
+group by occupation
+order by count(occupation);
+
+-- Output:
+-- Aamina(D) 
+-- Ashley(P) 
+-- Belvet(P) 
+-- Britney(P) 
+-- Christeen(S) 
+-- Eve(A) 
+-- Jane(S) 
+-- Jennifer(A) 
+-- Jenny(S) 
+-- Julia(D) 
+-- Ketty(A) 
+-- Kristeen(S) 
+-- Maria(P) 
+-- Meera(P) 
+-- Naomi(P) 
+-- Priya(D) 
+-- Priyanka(P) 
+-- Samantha(A) 
+-- There are a total of 3 doctors. 
+-- There are a total of 4 actors. 
+-- There are a total of 4 singers. 
+-- There are a total of 7 professors.
